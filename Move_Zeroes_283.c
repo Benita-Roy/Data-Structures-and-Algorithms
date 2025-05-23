a75aba65fc3bc962128b224e9bd3976c8db463ff
Benita-Roy <benitaroyofficial@gmail.com>
2025-05-23 21:03:53 +0530

    Update Move_Zeroes_283.c

--- Move_Zeroes_283.c
+++ Move_Zeroes_283.c
@@ -1,6 +1,6 @@
-//LEETCODE Q:283
-//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-//Note that you must do this in-place without making a copy of the array.
+//LEETCODE Question:283 Difficulty:Easy
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.*/
 void moveZeroes(int* nums, int numsSize) 
 {
     int i=0;
