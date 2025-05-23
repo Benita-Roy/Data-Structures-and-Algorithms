29d9985ef7414609d4ae9affeafd529e53b3bde7
Benita-Roy <benitaroyofficial@gmail.com>
2025-05-23 21:09:52 +0530

    Update Move_Zeroes_283.c

--- Move_Zeroes_283.c
+++ Move_Zeroes_283.c
@@ -3,12 +3,17 @@
 Note that you must do this in-place without making a copy of the array.*/
 void moveZeroes(int* nums, int numsSize) 
 {
+    //initialise two variables to keep track of the positions of zero and non zero elements to later exchange their positions
     int i=0;
     int j=0;
+
+    //repeat while i and j are both within bounds and i is less than j
     while(i<numsSize&& j<numsSize && i<=j)
     {
+        //check if the ith element is zero
         if(nums[i]==0)
-        {
+        {    
+            //check if the jth element is also zero in which case j is incremented till a non-zero element is found
             if(nums[j]==0)
             {
                 j++;
@@ -16,12 +21,14 @@ void moveZeroes(int* nums, int numsSize)
 
             else
             {
+                //swap the non-zero element(j) with the zero element(i)
                 int temp=nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
             }
         }
 
+        //if the ith element is non zero then increment both i and j to possibly find the next zero
         else
         {
             i++;
