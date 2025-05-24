561a1cc21d4566551aac45ff7c3d55ad77cb1661
Benita Roy <benitaroyofficial@gmail.com>
2025-05-24 14:21:54 +0530

    Update Remove_Duplicates_From_Sorted_Array_26.c

--- Remove_Duplicates_From_Sorted_Array_26.c
+++ Remove_Duplicates_From_Sorted_Array_26.c
@@ -1,8 +1,14 @@
+
 int removeDuplicates(int* nums, int n) {
-    int i;
+    
+    //to count the number of unique elements in the given array
     int count=0;
+    
+    //the flag is stratergically chosen as the highest value element in the array plus 1(given it is sorted)
     int flag=nums[n-1]+1;
 
+    //replacing duplicates by the flag in 0(n)
+    int i;
     for(i=1;i<n;i++)
     {
         int j=i-1;
@@ -12,6 +18,7 @@ int removeDuplicates(int* nums, int n) {
         }
     } 
 
+    //complexity can be improved by a large extent by changing the sorting method used below - now O(n^2)
     for(i=0;i<n;i++)
     {
         for(int j=i+1;j<n;j++)
@@ -25,6 +32,7 @@ int removeDuplicates(int* nums, int n) {
         }
     }
 
+    //counting the unique elements (can be done early on)
     for(i=0;i<n;i++)
     {   
         if(nums[i]!=flag)
