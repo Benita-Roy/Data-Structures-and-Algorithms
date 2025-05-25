7db61b8716f3c7f973a3bee327a7d256d991fe35
Benita Roy <benitaroyofficial@gmail.com>
2025-05-25 06:44:44 +0530

    Update Best_Time_To_Buy_And_Sell_Stock_121.c
    Changed time complexity from O(n^2) to O(n) - "Kadane's Algorithm variant" aka "Single-pass greedy algorithm" for the "Best Time to Buy and Sell Stock" problem.

--- Best_Time_To_Buy_And_Sell_Stock_121.c
+++ Best_Time_To_Buy_And_Sell_Stock_121.c
@@ -1,15 +1,24 @@
+//LEETCODE
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
 int maxProfit(int* prices, int n) {
+    int min_price=prices[0];
     int max_profit=0;
-    for(int i=0;i<n;i++)
+
+    for(int i=1;i<n;i++)
     {
-        int profit;
-        for(int j=i+1;j<n;j++)
+        int profit = prices[i]-min_price;
+        if(profit>max_profit)
         {
-            profit=prices[j]-prices[i];
-            if(profit>max_profit)
-            {
-                max_profit=profit;
-            }
+            max_profit=profit;
+        }
+
+        if(prices[i]<min_price)
+        {
+            min_price=prices[i];
         }
     }
 
