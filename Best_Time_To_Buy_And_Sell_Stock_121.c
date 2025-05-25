e2956420986eae0df6e187ea0cd6be1e654b9d65
Benita Roy <benitaroyofficial@gmail.com>
2025-05-25 06:49:53 +0530

    Update Best_Time_To_Buy_And_Sell_Stock_121.c

--- Best_Time_To_Buy_And_Sell_Stock_121.c
+++ Best_Time_To_Buy_And_Sell_Stock_121.c
@@ -4,7 +4,6 @@
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
-
 int maxProfit(int* prices, int n) {
     //keeping track of the global minimum price
     int min_price=prices[0];
