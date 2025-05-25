30ce2a96ed5e415d5afcf51e1974e85a0f776820
Benita Roy <benitaroyofficial@gmail.com>
2025-05-25 06:49:14 +0530

    Update Best_Time_To_Buy_And_Sell_Stock_121.c

--- Best_Time_To_Buy_And_Sell_Stock_121.c
+++ Best_Time_To_Buy_And_Sell_Stock_121.c
@@ -4,18 +4,26 @@
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.*/
+
 int maxProfit(int* prices, int n) {
+    //keeping track of the global minimum price
     int min_price=prices[0];
+    //keeping track of the global maximum profit
     int max_profit=0;
 
+    //single pass algorithm i.e. in O(n) time
     for(int i=1;i<n;i++)
-    {
+    {    
+        //computing maximum possible profit an checking if it beats the global maximum profit 
         int profit = prices[i]-min_price;
+
+        //if the profit is greater then update the max_profit 
         if(profit>max_profit)
         {
             max_profit=profit;
         }
 
+        //update the global minimum price is current price of stock is lower
         if(prices[i]<min_price)
         {
             min_price=prices[i];
