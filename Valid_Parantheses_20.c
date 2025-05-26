1c4ee87172293d037de303fa0d4ad3dc9e9b970b
Benita Roy <benitaroyofficial@gmail.com>
2025-05-26 17:01:17 +0530

    Update Valid_Parantheses_20.c

--- Valid_Parantheses_20.c
+++ Valid_Parantheses_20.c
@@ -1,17 +1,20 @@
+/*Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.*/
 #include <stdbool.h>
 bool isValid(char* s) {
     char stack[strlen(s)];
     int top=-1;
-    
-
-    
     for(int i=0;i<strlen(s);i++)
     {   
         if(s[i]=='('||s[i]=='['||s[i]=='{')
         {
             top++;
             stack[top]=s[i];
-      
         }
 
         else if(top!=-1)
