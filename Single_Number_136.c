38c0ca0bb7fcac3ff9d5aee8e6e3a24043ac8f49
Benita Roy <benitaroyofficial@gmail.com>
2025-05-26 16:01:16 +0530

    Update Single_Number_136.c

--- Single_Number_136.c
+++ Single_Number_136.c
@@ -1,3 +1,8 @@
+//LEETCODE Question:136 Difficulty:Easy
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.*/
+
+/*--NEEDED TO LEARN ABOUT THIS XOR PROPERTY TO IMPLEMENT THIS IN THE REQUIRED COMPLEXITY--*/
 int singleNumber(int* nums, int numsSize) {
     int result=0;
     for(int i=0;i<numsSize;i++)
