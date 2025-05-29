a49e711205a64d00e20651ac36e250b1ec8154c9
Benita Roy <benitaroyofficial@gmail.com>
2025-05-30 05:22:05 +0530

    Add files via upload

---
+++ Rotate_Array_189.c
@@ -0,0 +1,20 @@
+void reverse(int * arr,int x,int y)
+{   
+    int start=x;
+    int end=y;
+    while(start<end)
+    {
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+        k=k%numsSize;
+        reverse(nums,0,numsSize-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,numsSize-1);
+}
