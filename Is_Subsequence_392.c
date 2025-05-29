70b4b27610c3ccd789dc0ac98b9316ce332fe497
Benita Roy <benitaroyofficial@gmail.com>
2025-05-29 16:28:50 +0530

    Add files via upload

---
+++ Is_Subsequence_392.c
@@ -0,0 +1,25 @@
+#include<stdbool.h>
+bool isSubsequence(char* s, char* t) {
+    int i=0;
+    int j=0;
+    while(j<strlen(t))
+    {
+        if(s[i]==t[j])
+        {
+            i++;
+            j++;
+        }
+
+        else
+        {
+            j++;
+        }
+    }
+
+    if(i==strlen(s))
+    {
+        return true;
+    }
+
+    return false;
+}
