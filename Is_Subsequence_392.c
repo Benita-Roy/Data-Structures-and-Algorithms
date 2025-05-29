ce715465a7c719a26a485fa3ca086d64f17edc09
Benita Roy <benitaroyofficial@gmail.com>
2025-05-29 16:33:12 +0530

    Update Is_Subsequence_392.c

--- Is_Subsequence_392.c
+++ Is_Subsequence_392.c
@@ -4,26 +4,33 @@
 A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).*/
 #include<stdbool.h>
 bool isSubsequence(char* s, char* t) {
+    //to track position in string t 
     int i=0;
+    //to track position in string s
     int j=0;
+    //iterate over string t 
     while(j<strlen(t))
     {
+        //if the characters match, move forward in both strings
         if(s[i]==t[j])
         {
             i++;
             j++;
         }
 
+        //if the characters don't match, move forward in only the string t to see if there is a match elsewhere
         else
         {
             j++;
         }
     }
 
+    //if at the end the entire pattern was a match then return true 
     if(i==strlen(s))
     {
         return true;
     }
 
+    //else return false
     return false;
 }
