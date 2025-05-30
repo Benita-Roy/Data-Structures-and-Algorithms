7d3e29d1f8f0a349e31833c35c4b3287c1bc8699
Benita Roy <benitaroyofficial@gmail.com>
2025-05-30 18:44:51 +0530

    Update Product_of_Array_Except_Self_238.c

--- Product_of_Array_Except_Self_238.c
+++ Product_of_Array_Except_Self_238.c
@@ -9,20 +9,25 @@ You must write an algorithm that runs in O(n) time and without using the divisio
 */
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
     int* output=(int*)malloc(sizeof(int)*numsSize);
-     
+
+    //for the first element prefix product is 1 since there is no element before it 
     output[0]=1;
+    //first pass to find prefix prdouct
     for(int i=1;i<numsSize;i++)
     {
         output[i]=output[i-1]*nums[i-1];
     }
 
+    //suffix product for the last element is one
     int suffix=1;
+    //second pass to find suffix product
     for(int i=numsSize-1;i>=0;i--)
     {
         output[i]*=suffix;
         suffix*=nums[i];
     }
-    
+
+    //return the output array size to the caller
     *returnSize=numsSize;
     return output;
 }
