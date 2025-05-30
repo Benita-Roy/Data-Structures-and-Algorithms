c4792a365c54d6c16114f6458df502bf7b6e84ef
Benita Roy <benitaroyofficial@gmail.com>
2025-05-30 18:42:21 +0530

    Update Product_of_Array_Except_Self_238.c

--- Product_of_Array_Except_Self_238.c
+++ Product_of_Array_Except_Self_238.c
@@ -1,6 +1,12 @@
+//LEETCODE Question:238 Difficulty:Medium
+/*Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.*/
 /**
  * Note: The returned array must be malloced, assume caller calls free().
- */
+*/
 int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
     int* output=(int*)malloc(sizeof(int)*numsSize);
      
