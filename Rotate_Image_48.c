71b5dd859c117bea6735dfceef28e33b9ed3bba8
Benita Roy <benitaroyofficial@gmail.com>
2025-06-01 17:57:12 +0530

    Update Rotate_Image_48.c

--- Rotate_Image_48.c
+++ Rotate_Image_48.c
@@ -4,6 +4,8 @@
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.*/
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
     int temp;
+    
+    //swap ith row and jth column element with jth row and ith column element 
     for(int i=0;i<matrixSize;i++)
     {
         for(int j=i+1;j<*matrixColSize;j++)
@@ -13,11 +15,13 @@ void rotate(int** matrix, int matrixSize, int* matrixColSize) {
             matrix[j][i]=temp;
         }
     }
+
+    //reverse the row elements 
     for (int i=0;i<matrixSize;i++)
     {
         for(int j=0;j<*matrixColSize/2;j++)
         {
-            int temp=matrix[i][j];
+            temp=matrix[i][j];
             matrix[i][j]=matrix[i][matrixSize-1-j];
             matrix[i][matrixSize-1-j]=temp;
         }
